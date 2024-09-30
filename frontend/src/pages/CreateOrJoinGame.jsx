fecdaae979937f83e54e66928f145ab5129b4d8f
BlindManCry <davit.avtandilashvili4@gmail.com>
2024-09-30 07:17:57 +0400

    update visuals

--- frontend/src/pages/CreateOrJoinGame.jsx
+++ frontend/src/pages/CreateOrJoinGame.jsx
@@ -4,9 +4,9 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import socket from "../Socket";
 import { useBluff } from "../context/BluffContext";
+import { FaGamepad, FaDice } from "react-icons/fa";
 
 function CreateOrJoinGame() {
-  const [create, setCreate] = useState(false);
   const [joinCode, setJoinCode] = useState("");
   const tableCode = generateRandomString();
   const navigate = useNavigate();
@@ -17,7 +17,6 @@ function CreateOrJoinGame() {
   };
 
   const handleCreate = () => {
-    setCreate(true);
     socket.emit("joinGame", tableCode, username, "CREATE");
     setActions((actions) => [...actions, `${username} დაკავშირდა`]);
     setTableCode(tableCode);
@@ -26,7 +25,7 @@ function CreateOrJoinGame() {
 
   return (
     <StyledCreateOrJoinGame>
-      <div>
+      {/* <div>
         {!create && <p onClick={handleCreate}>Create</p>}
         {create && tableCode}
       </div>
@@ -38,11 +37,150 @@ function CreateOrJoinGame() {
           value={joinCode}
         />
         <button onClick={handleJoin}>დადასტურება</button>
-      </div>
+      </div> */}
+      <CreateOrJoinDiv>
+        <CreateGame>
+          <h2>ახალი თამაში</h2>
+          <div>
+            <FaGamepad size={200} color="grey" />
+          </div>
+          <button onClick={handleCreate}>თამაშის შექმნა</button>
+        </CreateGame>
+        <DevideDiv>
+          <div></div>
+          <p>O</p>
+          <p>R</p>
+          <div></div>
+        </DevideDiv>
+        <JoinGame>
+          <h2>თამაშში შესვლა</h2>
+          <div>
+            <FaDice size={200} color="grey" />
+          </div>
+          <button onClick={handleJoin}>თამაშში შესვლა</button>
+          <input
+            type="text"
+            placeholder="შეიყვანე კოდი"
+            onChange={(e) => setJoinCode(e.target.value)}
+          />
+        </JoinGame>
+      </CreateOrJoinDiv>
     </StyledCreateOrJoinGame>
   );
 }
 
 export default CreateOrJoinGame;
 
-const StyledCreateOrJoinGame = styled.div``;
+const StyledCreateOrJoinGame = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #000000bf;
+`;
+
+const CreateOrJoinDiv = styled.div`
+  width: 500px;
+  padding: 40px 20px 40px 20px;
+  background-color: white;
+  border-radius: 10px;
+  color: black;
+  display: flex;
+  gap: 20px;
+`;
+
+const CreateGame = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  & > h2 {
+    font-size: 18px;
+    color: #ff3c00;
+    font-weight: 600;
+  }
+
+  & > button {
+    width: 130px;
+    height: 45px;
+    margin: auto;
+    cursor: pointer;
+    background-color: purple;
+    color: white;
+    font-weight: bold;
+    border-radius: 12px;
+    border: none;
+    transition: all 0.2s ease-in-out;
+
+    &:hover {
+      background-color: #ffa500;
+      box-shadow: 0 0 10px rgba(255, 165, 0, 0.6);
+      transform: scale(1.1);
+    }
+  }
+`;
+
+const DevideDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  & > div {
+    width: 100%;
+    width: 1px;
+    /* height: 70px; */
+    height: 100%;
+    border: 2px solid grey;
+  }
+`;
+
+const JoinGame = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  & > h2 {
+    font-size: 18px;
+    color: #ff3c00;
+    font-weight: 600;
+  }
+
+  & > input {
+    margin-top: 10px;
+    border-radius: 10px;
+    padding-left: 20px;
+    padding-right: 20px;
+    border: 1px solid #ccc;
+    background-color: #f9f9f9;
+    transition: background-color 0.2s ease-in-out;
+    height: 30px;
+
+    &:focus {
+      background-color: #eef5ff;
+    }
+  }
+
+  & > button {
+    width: 130px;
+    height: 45px;
+    margin: auto;
+    cursor: pointer;
+    background-color: purple;
+    color: white;
+    font-weight: bold;
+    border-radius: 12px;
+    border: none;
+    transition: all 0.2s ease-in-out;
+
+    &:hover {
+      background-color: #ffa500;
+      box-shadow: 0 0 10px rgba(255, 165, 0, 0.6);
+      transform: scale(1.1);
+    }
+  }
+`;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,7 @@
+import styled from "styled-components";
 import { useBluff } from "../context/BluffContext";
 import { useNavigate } from "react-router-dom";
+import bluffLogo from "/images/bleffLogo.png";
 
 function Register() {
   const navigate = useNavigate();
@@ -7,20 +9,102 @@ function Register() {
   const { username, setUsername } = useBluff();
 
   const handleEnter = () => {
+    if (username.length === 0) return;
     navigate("/preGame");
   };
 
   return (
-    <div style={{ display: "flex", gap: "30px" }}>
-      <p>სახელი:</p>
-      <input
-        type="text"
-        onChange={(e) => setUsername(e.target.value)}
-        value={username}
-      />
-      <button onClick={() => handleEnter(username)}>ენთერი</button>
-    </div>
+    <StyledRegister>
+      <RegisterDiv>
+        <img src={bluffLogo} alt="" />
+        <div>
+          <p>ჩაწერეთ სახელი:</p>
+          <input
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+          />
+        </div>
+        <button onClick={() => handleEnter(username)}>შესვლა</button>
+      </RegisterDiv>
+    </StyledRegister>
   );
 }
 
 export default Register;
+
+const StyledRegister = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #000000bf;
+`;
+
+const RegisterDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 600px;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #ffffff;
+  border-radius: 10px;
+  box-shadow: 0 8px 20px rgba(0, 0, 0, 0.6);
+
+  & > img {
+    width: 240px;
+    height: 240px;
+    border-radius: 20px;
+    object-fit: cover;
+    box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+  }
+
+  & > div {
+    display: flex;
+    gap: 30px;
+    margin-top: 40px;
+  }
+
+  & > div > p {
+    color: #333333;
+    font-weight: 500;
+  }
+
+  & > div > input {
+    border-radius: 10px;
+    padding-left: 20px;
+    padding-right: 20px;
+    border: 1px solid #ccc;
+    background-color: #f9f9f9;
+    transition: background-color 0.2s ease-in-out;
+
+    &:focus {
+      background-color: #eef5ff;
+    }
+  }
+
+  & > button {
+    width: 100px;
+    height: 45px;
+    cursor: pointer;
+    margin-top: 30px;
+    background-color: purple;
+    color: white;
+    font-weight: bold;
+    border-radius: 12px;
+    border: none;
+    transition: all 0.2s ease-in-out;
+
+    &:hover {
+      background-color: #ffa500;
+      box-shadow: 0 0 10px rgba(255, 165, 0, 0.6);
+      transform: scale(1.1);
+    }
+  }
+`;
